eb773ed04aac47a1035431c3fb6fc730ea548963
Rastvorov Sergei <144595454+krevetka-is-afk@users.noreply.github.com>
2026-01-06 14:26:33 +0300

    rest framework & response

--- learnproj/base/views.py
+++ learnproj/base/views.py
@@ -1,11 +1,26 @@
 from django.forms.models import model_to_dict
-from django.http import JsonResponse
+
+# from django.http import JsonResponse
 from products.models import Product
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
+    """
+    Docstring for api_home
+
+    :param request: Description
+    :param args: Description
+    :param kwargs: Description
+    """
+
+    # if request.method != "POST":
+    #     return Response({"detail": "GET method not allowed"}, status=405)
+
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
         data = model_to_dict(model_data, fields=["id", "title", "price"])
-    return JsonResponse({"data": data})
+    return Response({"data": data})
