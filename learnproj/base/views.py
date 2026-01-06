571bc8ee0df354d9c7cbb4baf77278addd2edbb7
Rastvorov Sergei <144595454+krevetka-is-afk@users.noreply.github.com>
2026-01-06 14:02:05 +0300

    django model instance as api response

--- learnproj/base/views.py
+++ learnproj/base/views.py
@@ -1,22 +1,16 @@
-import json
-
 from django.http import JsonResponse
+from products.models import Product
 
 
 def api_home(request, *args, **kwargs):
-    # request -> HttpRequest -> Django
-    body = request.body  # byte string of JSON data
-    print(request.GET)
-    print(request.POST)
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body)  # json string -> dict
-
-    except BaseException:
-        print("ERR: can not loads json")
-
-    print(data)
-    data["params"] = dict(request.GET)
-    data["headers"] = dict(request.headers)  # request.META -> headers dict
-    data["content_type"] = request.content_type
+    if model_data:
+        data["id"] = model_data.id
+        data["title"] = model_data.title
+        data["content"] = model_data.content
+        data["price"] = model_data.price
+        # model instance (model_data)
+        # turn a python dict
+        # return a json to client
     return JsonResponse({"message": "Django api response", "data": data})
---
+++ uv-linters.sh
@@ -0,0 +1,5 @@
+uv run ruff check --fix .
+uv run black .
+uv run isort .
+uv run pytest -q
+uv run pre-commit run --all-files
