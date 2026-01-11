b5a88266279e9d0c216a15a82cd6e7740f277e2f
Rastvorov Sergei <144595454+krevetka-is-afk@users.noreply.github.com>
2026-01-11 18:10:23 +0300

    urls, reverse, serializers

--- learnproj/products/serializers.py
+++ learnproj/products/serializers.py
@@ -1,14 +1,47 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
 from .models import Product
 
 
 class PrimaryProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
+    url = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
+    url_ = serializers.HyperlinkedIdentityField(view_name="product-detail", lookup_field="pk")
 
     class Meta:
         model = Product
-        fields = ["pk", "title", "content", "price", "sale_price", "get_discount", "my_discount"]
+        fields = [
+            "url",
+            "url_",
+            "edit_url",
+            "pk",
+            "title",
+            "content",
+            "price",
+            "sale_price",
+            "get_discount",
+            "my_discount",
+        ]
+
+    def get_url(self, obj):
+        # return f'/base/products/{obj.pk}/'
+        request = self.context.get("request")
+
+        if request is None:
+            return None
+
+        return reverse("product-detail", kwargs={"pk": obj.pk}, request=request)
+
+    def get_edit_url(self, obj):
+        # return f'/base/products/{obj.pk}/'
+        request = self.context.get("request")
+
+        if request is None:
+            return None
+
+        return reverse("product-edit", kwargs={"pk": obj.pk}, request=request)
 
     def get_my_discount(self, obj):
         # obj.user -> user.username
